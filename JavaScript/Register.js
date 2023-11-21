cc9595f2ea00b2a900d4071ba34235a8650bb9a6
RetajK <151464985+RetajK@users.noreply.github.com>
2023-11-21 21:23:13 +0300

    updated

---
+++ JavaScript/Register.js
@@ -0,0 +1,43 @@
+
+function validateForm() {
+    
+        
+    // Retrieve form inputs
+    var kidName = document.getElementById("kidName");
+    var kidDOB = document.getElementById("kidDOB");
+    var kidGender = document.getElementById("kidGender");
+    var kidPhone = document.getElementById("kidPhone");
+        
+    // Validate inputs
+    if (kidName ==""||kidDOB ==""||kidGender ==""|| kidPhone =="") {
+      alert("Please fill in all fields.");
+      return false;
+    }
+  
+    if (!/^[a-zA-Z][a-zA-Z\s]*$/.test(kidName)) {
+      alert("Invalid name. Name should not start with a number.");
+      return false ;
+    }
+  
+    if (kidPhone.length !== 10 || isNaN(kidPhone)) {
+      alert("Invalid phone number. Phone number should be 10 digits.");
+      return false;
+    }
+  
+    var currentYear = new Date().getFullYear();
+    var kidYear = new Date(kidDOB).getFullYear();
+    if (kidYear > currentYear - 6) {
+      alert("Invalid date of birth. Children younger than 6 years old are not accepted.");
+      return false ;
+    }} 
+
+    function myfunction(){
+      var childName = registerForm.kidName.value;
+      localStorage.setItem("childName", kidName);
+    }
+    
+
+   
+
+
+  
